faf5500e2bd59bbed7466f61a06456172068c048
vandeurenglenn <vandeurenglenn@gmail.com>
2023-11-05 16:01:05 +0100

    Update tsConfig & export types

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import typescript from '@rollup/plugin-typescript';
-import tsconfig from './tsconfig.json' assert { type: 'json' }
 
 export default [{
   input: ['src/index.ts', 'src/codecs.ts', 'src/utils.ts'],
@@ -8,6 +7,6 @@ export default [{
     format: 'es'
   },
   plugins: [
-    typescript(tsconfig)
+    typescript()
   ]
 }]
